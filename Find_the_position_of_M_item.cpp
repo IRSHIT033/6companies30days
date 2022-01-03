eb88d95ca9410379780727a55a37cea4297b13b7
IRSHIT033 <83210491+IRSHIT033@users.noreply.github.com>
2022-01-03 21:06:09 +0530

    Create Find_the_position_of_M_item.cpp

---
+++ Find_the_position_of_M_item.cpp
@@ -0,0 +1,33 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+class Solution {
+  public:
+    int findPosition(int N , int M , int K) {
+        if(N==1)
+           return 1;
+       int ans=M%N+K-1;
+       if(ans==N)
+           return ans;
+       else
+           return ans%N;
+    }
+};
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N,M,K;
+        
+        cin>>N>>M>>K;
+
+        Solution ob;
+        cout << ob.findPosition(N,M,K) << endl;
+    }
+    return 0;
+}  
