a18466319859966827da9953b18a8becac02f2ea
IRSHIT033 <83210491+IRSHIT033@users.noreply.github.com>
2022-01-16 20:11:32 +0530

    Create Column_name_from_a_given_column_number.cpp

---
+++ Amazon/Column_name_from_a_given_column_number.cpp
@@ -0,0 +1,33 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution{
+    public:
+    string colName (long long int n)
+    {
+       string s="";
+       while(n){
+           char ch='A'+(n-1)%26;
+           s=ch+s;
+           n=(n-1)/26;
+       }
+       return s;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+	{
+		long long int n; cin >> n;
+		Solution ob;
+		cout << ob.colName (n) << '\n';
+	}
+}
+  // } Driver Code Ends
