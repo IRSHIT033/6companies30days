e33117c0e0fe0aa9d68b74f3c8e5053ab15ef47e
IRSHIT033 <83210491+IRSHIT033@users.noreply.github.com>
2022-01-28 04:11:47 +0530

    Create Minimum_steps_to_destination.cpp

---
+++ Microsoft/Minimum_steps_to_destination.cpp
@@ -0,0 +1,46 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+
+ 
+ 
+    int minSteps(int D)
+    {
+       if(D==0)return 0;
+       int sum=0,steps=0;
+       D=abs(D);
+       while(sum<D){
+           sum+=steps;
+           steps++;
+       }
+       
+       while((sum-D)%2==1){
+         sum+=steps;
+           steps++;   
+       }
+       return steps-1;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int D;
+        cin>>D;
+        
+        Solution ob;
+        cout<<ob.minSteps(D)<<"\n";
+    }
+    return 0;
+}  // } Driver Code Ends
