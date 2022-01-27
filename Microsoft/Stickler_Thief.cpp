65af355f8c4c2154cb3168dbee65ff721b27f874
IRSHIT033 <83210491+IRSHIT033@users.noreply.github.com>
2022-01-28 00:15:37 +0530

    Create Stickler_Thief.cpp

---
+++ Microsoft/Stickler_Thief.cpp
@@ -0,0 +1,50 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the maximum money the thief can get.
+    int FindMaxSum(int arr[], int n)
+    {
+        // Your code here
+        if(n==1)return arr[0];
+        int a=0;
+        int b=arr[0];
+        
+        for(int i=2;i<=n;i++)
+        {
+            int c=max(b,a+arr[i-1]);
+            a=b;
+            b=c;
+        }
+        return b;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    //taking total testcases
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    //taking number of houses
+		int n;
+		cin>>n;
+		int a[n];
+		
+		//inserting money of each house in the array
+		for(int i=0;i<n;++i)
+			cin>>a[i];
+		Solution ob;
+		//calling function FindMaxSum()
+		cout<<ob.FindMaxSum(a,n)<<endl;
+	}
+	return 0;
+}
+  // } Driver Code Ends
