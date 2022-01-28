45b66088d830790edd1bd07f49ca5b100b23c3ab
IRSHIT033 <83210491+IRSHIT033@users.noreply.github.com>
2022-01-28 20:39:50 +0530

    Create Number_of_distict_Words_with_k_maximum_contiguou_vowels.cpp

---
+++ Adobe/Number_of_distict_Words_with_k_maximum_contiguou_vowels.cpp
@@ -0,0 +1,56 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+  public:
+  #define ll long long int
+  
+  const int mod=1e9+7;
+  
+    int kvowelwords(int N, int K) {
+        // Write Your Code here
+        vector<vector<ll>>dp(N+1,vector<ll>(K+1));
+        
+        for(int i=0;i<=N;i++){
+            for(int j=0;j<=K;j++){
+                if(i==0)
+                   dp[i][j]=1;
+                else{
+                   dp[i][j]=dp[i-1][K]*21 % mod;
+                   if(j>0) dp[i][j]=(dp[i][j]+dp[i-1][j-1]*5%mod) %mod;
+                   
+                }                    
+            }
+        }
+        return dp[N][K];
+        
+    }
+};
+
+
+// { Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;int K;
+        cin >>N>>K;
+        
+        Solution ob;
+        int ans = ob.kvowelwords(N,K);
+        cout << ans << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
