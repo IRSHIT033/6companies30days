f8a3a2f3a9474f9ab99e85e04cc1a7c2e0bb1da6
IRSHIT033 <83210491+IRSHIT033@users.noreply.github.com>
2022-01-28 19:36:09 +0530

    Create Longest_Arithmetic_Progression.cpp

---
+++ Adobe/Longest_Arithmetic_Progression.cpp
@@ -0,0 +1,54 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    int lengthOfLongestAP(int A[], int n) {
+        // code here
+        
+        if(n<=2)return n;
+        
+        vector<unordered_map<int,int>>dp(n);
+        
+        int max_len=2;
+        
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int diff=A[j]-A[i];
+                if(dp[i].find(diff)!=dp[i].end())
+                {
+                    dp[j][diff]=dp[i][diff]+1;
+                }
+                else{
+                    dp[j][diff]=2;
+                }
+                max_len=max(max_len,dp[j][diff]);
+            }
+        }
+        return max_len;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int A[n];
+        for (int i = 0; i < n; i++) {
+            cin >> A[i];
+        }
+        Solution ob;
+        auto ans = ob.lengthOfLongestAP(A, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}  // } Driver Code Ends
