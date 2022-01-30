037315e8cfe8ef1a950a76c7c57374f1f90fbdcd
IRSHIT033 <83210491+IRSHIT033@users.noreply.github.com>
2022-01-30 10:16:35 +0530

    Create Number_of_Unique_Paths.cpp

---
+++ Walmart/Number_of_Unique_Paths.cpp
@@ -0,0 +1,45 @@
+// { Driver Code Starts
+//Initial template for C++
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template in C++
+
+class Solution
+{
+    public:
+    //Function to find total number of unique paths.
+    int NumberOfPath(int a, int b)
+    {
+        //code here
+          int ans=1;
+       int x=a+b-2;
+       int y=min(a,b)-1;
+       for(int i=1;i<=y;++i){
+           ans=ans*(x-y+i)/i;
+       }
+       return ans;
+    }
+    
+};
+
+
+// { Driver Code Starts.
+int main()
+{
+    //taking total testcases
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        //taking dimensions of the matrix
+        int a,b;
+        cin>>a>>b;
+        Solution ob;
+        //calling NumberOfPath() function
+        cout << ob.NumberOfPath(a,b) << endl;
+    }
+}
+
+  // } Driver Code Ends
